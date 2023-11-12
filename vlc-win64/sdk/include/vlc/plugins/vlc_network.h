c7a35a4bfb9d31779b1fcbfdd5d0e24a66a33503
Rye Mutt <rye@alchemyviewer.org>
2023-11-12 16:00:16 -0500

    vendor binary 3.0.20

--- vlc-win64/sdk/include/vlc/plugins/vlc_network.h
+++ vlc-win64/sdk/include/vlc/plugins/vlc_network.h
@@ -3,7 +3,7 @@
  *****************************************************************************
  * Copyright (C) 2002-2005 VLC authors and VideoLAN
  * Copyright © 2006-2007 Rémi Denis-Courmont
- * $Id: 010454a01c09730b342d9603d2dc1770361057d2 $
+ * $Id$
  *
  * Authors: Christophe Massiot <massiot@via.ecp.fr>
  *          Laurent Aimar <fenrir@via.ecp.fr>
